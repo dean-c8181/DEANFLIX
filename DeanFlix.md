f2ac17ef548f133e667fceb3ef7a9672d8d5a602
Dean.C <dean-c8181@gmail.com>
2020-09-11 20:01:06 +0900

    DEANFLIX ver.1 - first Setting and set HashRouter

---
+++ DeanFlix.md
@@ -0,0 +1,10 @@
+# DeanFlix
+
+Learning React and ES6 by building a Movie Discovery App.
+
+## Screens
+
+- [ ] Home
+- [ ] TV
+- [ ] Search
+- [ ] Detail
---
+++ src/Components/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "../Routes/Home";
+import tv from "../Routes/TV";
+import Search from "../Routes/Search";
+
+export default () => (
+    <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={tv} />
+        <Route path="/search" exact component={Search} />
+    </Router>
+)
