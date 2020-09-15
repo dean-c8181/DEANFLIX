eba55d3d95fae04cbff150d22dd83fcb8097ebb9
Dean.C <dean-c8181@gmail.com>
2020-09-15 18:33:32 +0900

    DEANFLIX ver.6// Global styled-components 만들기; styled-reset;  공통(Global)css작업과 Header CSS작업

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Router from "./Router";
+import GlobalStyles from "./GlobalStyles";
 
 // Link는 Router안에 있어야 하기 때문에 Header.Js의 컴포넌트를 Router안에서 Import하게 변경
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
 
       </Router>
+      <GlobalStyles></GlobalStyles>
     </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,16 +11,52 @@ import styled from "styled-components";
 // react Router에는 href가 없고 to로 사용한다.
 // Link는 Router안에 있어야 하기 때문에 Header.Js의 컴포넌트를 Router안에서 Import하게 변경
 
-const Header = styled.header``;
+
+// Styled-componets의 style자동완성 애드온 설치 vscode-styled-components
+// https://marketplace.visualstudio.com/items?itemName=jpoissonnier.vscode-styled-components 
+
+
+// SC(styled-components)를 Global로 설정하기.
+// 1. yarn add styled-reset
+// 2. js파일 생성 (GlobalStyles.js) >>> 해당파일로 이동하여 작업
+// 3. App.js에서 <GlobalStyles> 컴포넌트 추가
+
+const Header = styled.header`
+    color: #FFF;
+    position:fixed;
+    top:0;
+    left:0;
+    width:100%;
+    height:50px;
+    display:flex;
+    align-items:center;
+    background-color:rgba(20,20,20,1);
+    z-index:10;
+    box-shadow:0 1px 5px 2px rgba(0,0,0,0.8);
+`;
 
 const List = styled.ul`
     display:flex;
-    &:hover{background-color:blue;}
 `;
 
-const Items = styled.li``;
+const Items = styled.li`
+    margin-left:30px;    
+    text-align:center;
+    /*&:first-child{
+        margin-left:0;
+    }*/
+    
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+    display:block;
+    height:50px;
+    line-height:50px;
+    padding:0 5px;
+    &:hover{
+        color:magenta;
+    }
+`;
 
 export default () => (
     <Header>
