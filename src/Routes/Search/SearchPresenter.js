3c4801f73a3a9a856093cbb277e8393462a2a539
Dean.C <dean-c8181@gmail.com>
2020-09-18 17:54:07 +0900

    DEANFLIX ver.15// Presenter파일들 기본작업, PropTypes 작업.

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,16 @@
-export default () => "Search11";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({ movieResults, tvResults, handleSubmit, searchTerm, loading, error }) => null;
+
+SearchPresenter.propTypes = {
+    movieResults: PropTypes.array,
+    tvResults: PropTypes.array,
+    handleSubmit: PropTypes.func.isRequired,
+    searchTerm: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string    
+};
+
+export default SearchPresenter;
