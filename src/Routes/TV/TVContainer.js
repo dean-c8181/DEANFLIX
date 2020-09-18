d8749a8b004f750436a72ce9981d6d1e714d387a
Dean.C <dean-c8181@gmail.com>
2020-09-18 14:04:39 +0900

    DEANFLIX ver.11// 객체비구조화 할당(Object Destructuring = OD)과 TV Container 데이터작업.

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { tvApi } from "../../api";
 import TVPresenter from "./TVPresenter";
 
 export default class extends React.Component{
@@ -8,10 +9,28 @@ export default class extends React.Component{
         airingToday: null,
         loading: true,
         error: null
-    }
+    };
+
+    // 아래는 async componentDidMount(){} 와 같음
+     componentDidMount = async() => {
+        try{
+           const {data: {results: topRated}} = await tvApi.topRated();
+           const {data: {results: popular}} = await tvApi.popular();
+           const {data: {results: airingToday}} = await tvApi.airingToday();
+
+           this.setState({ topRated, popular, airingToday })
+        } catch{
+            this.setState({
+                error: "Cant find TV infomation."
+            })
+        } finally{
+            this.setState({ loading: false });
+        }
+    };
 
     render(){
         const { topRated, popular, airingToday, loading, error } = this.state;
+        console.log(this.state);
         return (
             <TVPresenter
                 topRated={topRated}
