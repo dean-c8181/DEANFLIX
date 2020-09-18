1c9c9cf99672eae19294c7d864bfc9272f4fcba7
Dean.C <dean-c8181@gmail.com>
2020-09-18 14:35:07 +0900

    DEANFLIX ver.12// Search Container 데이터작업, Detail 페이지를 위한 Router.js 기초작업.

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { moviesApi, tvApi } from "../../api";
 import SearchPresenter from "./SearchPresenter";
 
 export default class extends React.Component{
@@ -6,19 +7,50 @@ export default class extends React.Component{
         movieResults: null,
         tvResults: null,
         searchTerm: "",
-        loading: true,
+        loading: false,
         error: null
     }
 
+    // 텍스트의 빈칸을 확인하고 enter 검색
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+
+        if(searchTerm !== ""){
+            this.searchByTerm();
+        }
+
+    };
+
+    searchByTerm = async() => {
+        const { searchTerm } = this.state;
+        this.setState({ loading: true });
+        try{            
+            const {data: { results: movieResults }} = await moviesApi.search(searchTerm);
+            const {data: { results: tvResults }} = await tvApi.search(searchTerm);
+            //console.log(movieResults, tvResults);
+            this.setState({
+                movieResults,
+                tvResults
+            });
+        } catch{
+            this.setState({ error: "Can't find results." });
+        } finally{
+            this.setState({ loading: false });
+        }
+
+    };
+
     render(){
         const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+        //console.log(this.state);
         return (
             <SearchPresenter
             movieResults={movieResults}
             tvResults={tvResults}
             searchTerm={searchTerm}
             loading={loading}
-            error ={error }
+            error ={error}
+            handleSubmit={this.handleSubmit}
             >
 
             </SearchPresenter>
