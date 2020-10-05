1322ed29b82ccf09e7397449629b7985e40b14f6
Dean.C <dean-c8181@gmail.com>
2020-10-05 15:25:57 +0900

    DEANFLIX ver.21// Detail페이지 작업완료(영상정보)

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -40,6 +40,33 @@ const Cover = styled.div`
     border-radius:5px;
 `;
 
+const Data = styled.div`
+    width:70%;
+    padding-left:30px;
+`;
+
+const Title = styled.h3`
+    font-size:32px;
+    margin-bottom:10px;
+`;
+
+const ItemContainer = styled.div`
+    margin:20px 0;
+`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`
+    margin: 0 10px;
+`;
+
+const Overview = styled.p`
+    font-size:12px;
+    opacity:0.7;
+    line-height:1.5em;
+    text-align:justify;
+`;
+
 const DetailPresenter = ({ result, loading, error }) => (
     loading ? ( 
         <Loader/> 
@@ -52,6 +79,21 @@ const DetailPresenter = ({ result, loading, error }) => (
                 <Cover
                     bgImage={result.poster_path ? `https://image.tmdb.org/t/p/original${result.poster_path}` : require("../../assets/noPosterSmall.png")} 
                 />
+                <Data>
+                    <Title>{result.original_title ? result.original_title : result.original_name}</Title>
+                    <ItemContainer>
+                        <Item>{result.release_date ? result.release_date.substring(0,4) : result.first_air_date.substring(0,4)}</Item>
+                        <Divider>●</Divider>
+                        <Item>{result.runtime ? result.runtime : result.episode_run_time} min</Item>
+                        <Divider>●</Divider>
+                        <Item>
+                            {result.genres && result.genres.map((genre, index) => index === result.genres.length -1 ? genre.name : `${genre.name}/ `)}
+                        </Item>
+                    </ItemContainer>
+                    <Overview>
+                        {result.overview}
+                    </Overview>
+                </Data>
             </Content>
         </Container>
     )
